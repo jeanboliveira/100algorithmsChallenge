34778483208df9841f260a0d4a7473c5d063e4ca
Jean Oliveira <Jeanboliveira@outlook.com>
2019-02-13 15:52:30 -0600

    Completed adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,15 @@
-function adjacentElementsProduct(inputArray: number[]): number {
+function adjacentElementsProduct(inputArray: number[]): number
+{
+  let largestProduct = inputArray[0] * inputArray[1];
 
+  for (let i = 1; i < inputArray.length - 1; i++)
+  {
+    //Multiplies the array item to the item to its right
+    const product = inputArray[i] * inputArray[i + 1];
+    largestProduct = largestProduct < product ? product : largestProduct;
+  }
+
+  return largestProduct;
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
