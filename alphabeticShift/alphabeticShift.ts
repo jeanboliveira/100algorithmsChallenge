632a6a115b2b2fe293ce052b7b68ca3cb3624755
Jean Oliveira <jeanboliveira@outlook.com>
2019-02-15 15:58:13 -0600

    COmpleted alphabeticShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,4 +1,21 @@
-function alphabeticShift(inputString: string): string {
+function alphabeticShift(inputString: string): string 
+{
+    var reorderedString = [];
+
+    for (let i = 0; i < inputString.length; i++)
+    {
+        if (inputString[i] == 'z')
+        {
+            reorderedString[i] = 'a';
+        }
+        else
+        {
+            reorderedString.push(String.fromCharCode(inputString.charCodeAt(i) + 1));
+        }
+        
+    }
+
+    return reorderedString.join('');
 
 }
 
