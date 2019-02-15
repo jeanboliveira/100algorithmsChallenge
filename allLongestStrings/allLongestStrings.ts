8192c3009b38c121dbc682d53bc7b860705742c2
Jean Oliveira <jeanboliveira@outlook.com>
2019-02-15 13:13:53 -0600

    Completed allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,20 @@
-function allLongestStrings(inputArray: string[]): string[] {
+function allLongestStrings(inputArray: string[]): string[] 
+{
+    let longestLength = 0;
+    const longestWords = [];
+    
+    //Compares the length of each item in the inputArray and if that is larger, assign it to longestLength
+    inputArray.forEach((word : string) => {
+        longestLength = longestLength < word.length ? word.length : longestLength;
+    });
 
+    inputArray.forEach((word : string) => {
+        if(word.length === longestLength) {
+            longestWords.push(word);
+        }
+    });
+
+    return longestWords;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
