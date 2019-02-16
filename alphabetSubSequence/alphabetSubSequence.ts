929b74f59f1d6624f092ecc017e1ef766062c5b8
Jean Oliveira <jeanboliveira@outlook.com>
2019-02-15 21:16:20 -0600

    Completed alphabetSubSequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,7 +1,29 @@
-function alphabetSubsequence(s: string): boolean {
+function alphabetSubsequence(s: string): boolean 
+{
+    const chars: string[] = s.split('');
+    const charValues: number[] = [];
 
+    chars.forEach((char: string) => {
+        charValues.push(char.charCodeAt(0))
+    });
+
+    if (new Set(charValues).size !== charValues.length)
+    {
+        return false;
+    }
+
+    for (let i = 0; i < charValues.length; i++)
+    {
+        if (charValues[i] >= charValues[i + 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
 
+console.log(alphabetSubsequence('abaa'))
 console.log(alphabetSubsequence('zab'))
 console.log(alphabetSubsequence('effg'))
 console.log(alphabetSubsequence('cdce'))
